e5e3792b6d9dad0976eecbfcdfa87372c0e8022e
Joel Warrington <joel@homesuiteapartment.com>
2023-06-02 16:44:10 -0600

    Update messages and commands

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gemspec
 
 gem 'redis'
 gem 'discordrb', github: 'shardlab/discordrb', branch: 'main'
+gem 'chronic'
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.21"
--- lib/bot.rb
+++ lib/bot.rb
@@ -5,6 +5,7 @@ require 'securerandom'
 require 'discordrb'
 require 'discordrb/webhooks'
 require 'discordrb/api'
+require 'chronic'
 
 TOKEN = "change_me"
 SERVER_ID = 441743021654933515
@@ -15,11 +16,29 @@ DROOLSPOT_ID = 838610850893398016
 bot = Discordrb::Bot.new token: TOKEN, intents: %i[server_messages server_message_reactions]
 redis = Redis.new
 
+# bot.get_application_commands(server_id: SERVER_ID).each do |application|
+#   application.delete
+# end
+
 # move this to rake task
-bot.register_application_command(:beckon, 'Send out a beckon', server_id: SERVER_ID)
-bot.register_application_command(:play, 'Start the play', server_id: SERVER_ID)
+bot.register_application_command(:beckon, 'Send out a beckon', server_id: SERVER_ID) do |command|
+  command.string('start_time', 'When would the game start?')
+end
+bot.register_application_command(:play, 'Start', server_id: SERVER_ID) do |command|
+  command.string('best_of', 'How many games in the set?', choices: { 'BO1': '1', 'BO3': '3', 'BO5': '5', 'BO7': '7' })
+end
 
 bot.application_command(:beckon) do |event|
+  start_time = Chronic.parse(event.options['start_time'])
+
+  if start_time.nil? && !event.options['start_time'].nil?
+    revent.respond(content: ":x: Uh oh, looks like you didn't specify a valid start time.\n\nTry something like '5pm', 'tonight', 'now'")
+    return
+  elsif event.options['start_time'].nil?
+    start_time = Chronic.parse('tonight at 9')
+  end
+
+  event.respond(content: "Submitting a new beckon!", ephemeral: true)
   message = bot.send_message(
     event.channel,
     "<@&#{ROLE_ID}> a new beckon has appeared!",
@@ -28,7 +47,11 @@ bot.application_command(:beckon) do |event|
       color: 13632027,
       fields: [
         {
-          name: "<:coolspot:#{COOLSPOT_ID}>",
+          name: "**:stopwatch: Start Time**: #{start_time.strftime("%l:%M %p").strip}",
+          value: ""
+        },
+        {
+          name: "<:coolspot:#{COOLSPOT_ID}> | 0",
           value: "",
           inline: true
         },
@@ -38,10 +61,14 @@ bot.application_command(:beckon) do |event|
           inline: true
         },
         {
-          "name": "<:droolspot:#{DROOLSPOT_ID}>",
+          "name": "<:droolspot:#{DROOLSPOT_ID}> | 0",
           "value": "",
           "inline": true
-        }
+        },
+        {
+          name: "",
+          value: "",
+        },
       ]
     },
     nil,
@@ -50,11 +77,6 @@ bot.application_command(:beckon) do |event|
     }
   )
 
-  redis.set('beckon_channel_id', message.channel.id)
-  redis.set('beckon_id', message.id)
-
-
-  event.respond(content: 'Beckon has been sent!', ephemeral: true)
 
   message.react "coolspot:#{COOLSPOT_ID}"
   message.react "droolspot:#{DROOLSPOT_ID}"
@@ -70,32 +92,53 @@ bot.reaction_add do |event|
     object[emoji_name] = reactions.filter { |r| r['id'] != bot.bot_app.id.to_s }.map { |r| r['id'] }
   end
 
-  message_response = Discordrb::API::Channel.message("Bot #{TOKEN}", event.channel.id, event.message.id)
-  message = Discordrb::Message.new(JSON.parse(message_response.body), bot)
-
-  message.edit(
-    message.content,
+  event.message.edit(
+    event.message.content,
     {
       color: 13632027,
       fields: [
         {
-          name: "<:coolspot:#{COOLSPOT_ID}>",
-          value: users_by_emoji_reaction['coolspot'].map { |user| "<@#{user}>" }.join('\n'),
-          inline: true
+          name: event.message.embeds.first.fields.first.name,
+          value: event.message.embeds.first.fields.first.value,
+        },
+        {
+          name: "<:coolspot:#{COOLSPOT_ID}> | #{users_by_emoji_reaction['coolspot'].count}",
+          value: users_by_emoji_reaction['coolspot'].map { |user| "<@#{user}>" }.join("\n"),
+          inline: true,
         },
         {
           name: "",
           value: "",
-          inline: true
+          inline: true,
         },
         {
-          name: "<:droolspot:#{DROOLSPOT_ID}>",
-          value: users_by_emoji_reaction['droolspot'].map { |user| "<@#{user}>" }.join('\n'),
-          inline: true
+          name: "<:droolspot:#{DROOLSPOT_ID}> | #{users_by_emoji_reaction['droolspot'].count}",
+          value: users_by_emoji_reaction['droolspot'].map { |user| "<@#{user}>" }.join("\n"),
+          inline: true,
         }
       ]
     }
   )
+
+  users_by_emoji
+
+  if users_by_emoji_reaction['coolspot'].count >= 2
+    bot.send_message(
+      event.channel,
+      "**<@&#{ROLE_ID}> play time!**",
+      false,
+      nil,
+      nil,
+      {
+        roles: [ROLE_ID]
+      },
+      {
+        message_id: event.message.id,
+        channel_id: event.channel.id,
+        guild_id: event.server.id
+      }
+    )
+  end
 end
 
 bot.reaction_remove do |event|
@@ -117,8 +160,12 @@ bot.reaction_remove do |event|
       color: 13632027,
       fields: [
         {
-          name: "<:coolspot:#{COOLSPOT_ID}>",
-          value: users_by_emoji_reaction['coolspot'].map { |user| "<@#{user}>" }.join('\n'),
+          name: event.message.embeds.first.fields.first.name,
+          value: event.message.embeds.first.fields.first.value,
+        },
+        {
+          name: "<:coolspot:#{COOLSPOT_ID}> | #{users_by_emoji_reaction['coolspot'].count}",
+          value: users_by_emoji_reaction['coolspot'].map { |user| "<@#{user}>" }.join("\n"),
           inline: true
         },
         {
@@ -127,8 +174,8 @@ bot.reaction_remove do |event|
           inline: true
         },
         {
-          name: "<:droolspot:#{DROOLSPOT_ID}>",
-          value: users_by_emoji_reaction['droolspot'].map { |user| "<@#{user}>" }.join('\n'),
+          name: "<:droolspot:#{DROOLSPOT_ID}> | #{users_by_emoji_reaction['droolspot'].count}",
+          value: users_by_emoji_reaction['droolspot'].map { |user| "<@#{user}>" }.join("\n"),
           inline: true
         }
       ]
